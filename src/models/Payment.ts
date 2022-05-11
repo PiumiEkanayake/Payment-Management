7e32de0a5feacb4f4a33a9c4fe646c521e1df6be
Piumi <piumiekanayaka4@gmail.com>
2022-05-12 01:58:09 +0530

    Implement payment interface and model

---
+++ src/models/Payment.ts
@@ -0,0 +1,47 @@
+import {IPayment} from "../interfaces/IPayment";
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const PaymentSchema = new Schema(
+    {
+        chname:{
+            type:String,
+            required:false,
+            trim:true
+        },
+        cnum:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+    
+        cvc:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+        expiry:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+        type:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+        total:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+        date:{
+            type:Date,
+            required:false,
+            trim:true,
+        },
+    }
+
+
+
+);
+export default mongoose.model<IPayment & mongoose.Document>('payment',PaymentSchema)
